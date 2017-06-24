46e8c40c9917631de88de74825f4c60cf411609d
-robsonklima <-robsonklima@gmail.com>
2017-06-24 15:40:33 -0300

    new

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -21,9 +21,9 @@ switch(env) {
         var dbconfig = {
             connectionLimit : 100,
             host     : 'us-cdbr-iron-east-03.cleardb.net',
-            user     : 'be910e2a600c1a',
-            password : 'bc20dbc5',
-            database : 'heroku_6b75c71f60eab19',
+            user     : 'b81980c4d2abcf',
+            password : '7dfd7fcd',
+            database : 'heroku_92023a243cf1902',
             debug    :  false
         };
 }
