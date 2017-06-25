535873a82053bd439daeb4ed6930db12a76ebcba
-robsonklima <-robsonklima@gmail.com>
2017-06-25 19:32:16 -0300

    Send Location

--- routes/geolocations.js
+++ routes/geolocations.js
@@ -8,6 +8,22 @@ function ROUTER(router, pool) {
 ROUTER.prototype.handleRoutes = function(router, pool) {
     var self = this;
 
+    router.get("/geolocations", function(req, res) {
+        var query = "SELECT * FROM ??";
+        var vars = ["geolocations"];
+        query = mysql.format(query, vars);
+        pool.getConnection(function(err, connection) {
+            connection.query(query, function(err, geolocations) {
+                connection.release();
+                if(err) {
+                    res.status(400).send({"error": true, "details": err});
+                } else {
+                    res.status(200).send({"error": false,  geolocations});
+                }
+            });
+        });
+    });
+
     router.post("/geolocations", function(req, res) {
         var query = "INSERT INTO ?? (??,??,??) VALUES (?,?,?)";
         var vars = ["geolocations"
